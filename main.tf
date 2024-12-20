f98ed7bfa7d82db67fe97050e0b7759d4ccd0093
chcb1 <carlos1henrique@hotmail.com>
2024-12-20 03:35:15 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    linux = {
+      source = "TelkomIndonesia/linux"
+      version = "0.7.0"
+    }
+  }
+}
+
+provider "linux" {
+    host     = "192.168.235.131"
+    port     = 22
+    user     = "root"
+    password = "1"
+}
+
+resource "linux_file" "unnatifile" {
+    path = "/tmp/testdir"
+}
